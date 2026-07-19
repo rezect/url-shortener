86e9867227da32cbc5d3970da818e1d0bd654a23
rezect <rezectoff@gmail.com>
2026-07-19 16:09:21 +0300

    fix(models): update time fields to pointers for nullable database columns

--- internal/models/clicks.go
+++ internal/models/clicks.go
@@ -3,10 +3,10 @@ package models
 import "time"
 
 type Click struct {
-	Id        int64     `db:"id"`
-	ShortCode string    `db:"short_code"`
-	Ip        string    `db:"ip"`
-	UserAgent string    `db:"user_agent"`
-	Referer   string    `db:"referer"`
-	ClickedAt time.Time `db:"clicked_at"`
+	Id        int64      `db:"id"`
+	ShortCode string     `db:"short_code"`
+	Ip        string     `db:"ip"`
+	UserAgent string     `db:"user_agent"`
+	Referer   string     `db:"referer"`
+	ClickedAt *time.Time `db:"clicked_at"`
 }
